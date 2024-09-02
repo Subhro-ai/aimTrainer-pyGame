e0272e9b28b835cc16787f679b24eb5aa90e66fe
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-09-02 22:50:01 +0530

    Menu complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 white = (255,255,255)
+black = (0,0,0)
 HEIGHT = 720
 WIDTH = 1280
 pg.init()
@@ -8,6 +9,7 @@ clock = pg.time.Clock()
 pg.font.init()
 font = pg.font.Font("lemon_milk/LEMONMILK-Regular.otf", 42)
 running = True
+
 state = "menu"
 
 
@@ -20,10 +22,45 @@ def showMenu():
     screen.blit(title, pos)
 
     #Buttons
-    buttonRect = pg.Rect((WIDTH/2) - 100, 200, 200, 50)
-    button = pg.draw.rect(screen,white, buttonRect)
+    easyButtonRect = pg.Rect((WIDTH/2) - 200, 200, 400, 50)
+    medButtonRect = pg.Rect((WIDTH/2) - 200, 200 + 100, 400, 50)
+    hardButtonRect = pg.Rect((WIDTH/2) - 200, 200 + 200, 400, 50)
+    if easyButtonRect.collidepoint(mouse):
+        easyButtonRect = pg.Rect((WIDTH/2) - 200 -10, 200 - 10, 400, 50)
+        pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+    elif medButtonRect.collidepoint(mouse):
+        medButtonRect = pg.Rect((WIDTH/2) - 200 -10, 200 + 100 -10, 400, 50)
+        pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+    elif hardButtonRect.collidepoint(mouse):
+        hardButtonRect = pg.Rect((WIDTH/2) - 200 - 10, 200 + 200 -10, 400, 50)
+        pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+    else:
+        pg.mouse.set_cursor(pg.SYSTEM_CURSOR_ARROW)
+    
+
+    easyText = font.render("EASY", True, black , None)
+    medText = font.render("MEDIUM", True, black , None)
+    hardText = font.render("HARD", True, black , None)
+
+    easyButton = pg.draw.rect(screen,white, easyButtonRect)
+    easyRect = easyText.get_rect(center = easyButtonRect.center)
+    screen.blit(easyText, easyRect)
+
+    medButton = pg.draw.rect(screen, white, medButtonRect)
+    medRect = medText.get_rect(center = medButtonRect.center)
+    screen.blit(medText, medRect)
+
+    hardButton = pg.draw.rect(screen, white, hardButtonRect)
+    hardRect = hardText.get_rect(center = hardButtonRect.center)
+    screen.blit(hardText, hardRect)
+
+
+    
+
+
 
 while running:
+    mouse = pg.mouse.get_pos()
     
     for event in pg.event.get():
         if event.type == pg.QUIT:
